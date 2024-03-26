9006232876da56f1c04a1fef2e5a3d40a9835e1d
Kerubo0 <mmyers@kabarak.com>
2024-03-26 09:45:15 +0300

    modified the images and ensured responsiveness

--- app.js
+++ app.js
@@ -10,27 +10,31 @@ checkbox.addEventListener("change", () => {
   document.body.classList.toggle("dark");
 });*/
 
+
 function showSidebar() {
-    const sidebar = document.querySelector('.sidebar')
-    sidebar.style.display = 'flex';
+  const sidebar = document.querySelector('.sidebar')
+  sidebar.style.display = 'flex';
 }
 
 function hideSidebar() {
-    const sidebar = document.querySelector('.sidebar')
-    sidebar.style.display = 'none';
+  const sidebar = document.querySelector('.sidebar')
+  sidebar.style.display = 'none';
 }
 
 const backToTopBtn = document.getElementById("backToTopBtn");
 
 window.addEventListener("scroll", () => {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    backToTopBtn.style.display = "block";
-  } else {
-    backToTopBtn.style.display = "none";
-  }
+if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  backToTopBtn.style.display = "block";
+} else {
+  backToTopBtn.style.display = "none";
+}
 });
 
 backToTopBtn.addEventListener("click", () => {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+document.body.scrollTop = 0; // For Safari
+document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 });
+
+
+/*  TO BE DELETED  */
