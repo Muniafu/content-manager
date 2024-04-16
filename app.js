072ac3039bc526aa9cfc98791c5ece73fa3b5a22
Kerubo0 <mmyers@kabarak.com>
2024-04-16 09:06:44 +0300

    edited the dropdown

--- app.js
+++ app.js
@@ -27,5 +27,34 @@ document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 });
 
 
-/*  TO BE DELETED  */
- 
+// Get all elements with the class name 'angle'
+var angleImages = document.querySelectorAll('.angle');
+
+// Add click event listeners to each image
+angleImages.forEach(function(image) {
+    image.addEventListener('click', function() {
+        // Get the corresponding dropdown menu for this image
+        var dropdownMenu = this.parentElement.querySelector('.dropdown');
+
+        // Toggle the display property of the dropdown menu
+        if (dropdownMenu.style.display === "none" || dropdownMenu.style.display === "") {
+            dropdownMenu.style.display = "block";
+        } else {
+            dropdownMenu.style.display = "none";
+        }
+    });
+});
+
+var angleImages = document.querySelectorAll('.angle');
+const angleElem = document.getElementById("expand_less")
+ // Get the dropdown menu
+ var dropdownMenu = document.getElementById('dropdownMenu');
+
+  // Add click event listener to the expand less image
+  angleElem.addEventListener('click', function() {
+    // Hide the dropdown menu
+    dropdownMenu.style.display = "none";
+
+    // Show the expand more image
+    angleImages.style.display = "block";
+});
